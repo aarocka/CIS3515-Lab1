0c11d7b9df5a0f02f9c0d57da241b9ff02e7c705
Aaron <aarocka@protonmail.com>
2020-09-16 14:28:54 -0400

    moved hardcoded string to strings.xml

--- app/src/main/java/edu/temple/helloworld/MainActivity.java
+++ app/src/main/java/edu/temple/helloworld/MainActivity.java
@@ -12,8 +12,6 @@ public class MainActivity extends AppCompatActivity {
     TextView NewText;
     Button ChangeText;
 
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
