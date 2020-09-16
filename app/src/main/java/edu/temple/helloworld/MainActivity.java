075e3839804d9e1279852ab91c425382eabc3704
Aaron <aarocka@protonmail.com>
2020-09-16 14:27:22 -0400

    moved hardcoded string to strings.xml

--- app/src/main/java/edu/temple/helloworld/MainActivity.java
+++ app/src/main/java/edu/temple/helloworld/MainActivity.java
@@ -3,12 +3,32 @@ package edu.temple.helloworld;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView NewText;
+    Button ChangeText;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        NewText = findViewById(R.id.textView);
+        ChangeText = findViewById(R.id.button);
+
+        ChangeText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                NewText.setText(R.string.newText);
+            }
+        });
+
+
     }
+
 }
